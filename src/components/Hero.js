b59bb441769cc6a15082c585eca34e9933aa9ae8
Farshad Shadpour <farshad1353@gmail.com>
2024-02-25 20:20:49 +0100

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import { heroTitle } from '../data'
+
+const Hero = () => {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>{heroTitle}</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          explicabo debitis est autem dicta.
+        </p>
+        <a href="#tours" className="btn hero-btn">
+          explore tours
+        </a>
+      </div>
+    </section>
+  )
+}
+export default Hero
---
+++ src/data.js
@@ -0,0 +1,103 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+import tour5 from './images/tour-5.jpeg'
+import tour6 from './images/tour-6.jpeg'
+export const pageLinks = [
+  { id: 1, href: '#home', text: 'home' },
+  { id: 2, href: '#about', text: 'about' },
+  { id: 3, href: '#services', text: 'services' },
+  { id: 4, href: '#tours', text: 'tours' },
+]
+
+export const socialLinks = [
+  { id: 1, href: 'https://www.x.com', icon: 'fab fa-twitter' },
+  { id: 2, href: 'https://www.facebook.com', icon: 'fab fa-facebook' },
+  { id: 3, href: 'https://www.facebook.com', icon: 'fab fa-squarespace' },
+]
+
+export const heroTitle = 'my back road app'
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+]
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    location: 'china',
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    date: 'october 11th, 2020',
+    title: 'Best of java',
+    location: 'indonesia',
+    duration: 11,
+    cost: 1400,
+  },
+  {
+    id: 3,
+    image: tour3,
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    date: 'september 15th, 2020',
+    title: 'explore hong kong',
+    location: 'hong kong',
+    duration: 8,
+    cost: 5000,
+  },
+  {
+    id: 4,
+    image: tour4,
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    date: 'december 5th, 2019',
+    title: 'kenya highlights',
+    location: 'kenya',
+    duration: 20,
+    cost: 3300,
+  },
+  {
+    id: 5,
+    image: tour5,
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    location: 'china',
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 6,
+    image: tour6,
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    location: 'china',
+    duration: 6,
+    cost: 2100,
+  },
+]
